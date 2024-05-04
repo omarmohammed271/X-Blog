2a13a73762b91da20431b43e03b7cc020e97e072
moooooora <omarmohammed271@gmail.com>
2024-05-04 09:34:24 +0300

    Added base

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout
+from .models import Profile
 # Create your views here.
+
+def login_view(request):
+
+    return render(request,'accounts/login.html')
+
+def login_view(request):
+    logout(request)
+    return redirect('home')
+
+
